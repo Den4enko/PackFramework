5e8c6f6b7ec263f7963a50dfc1b52e7ae4b5872b
Dmytro Denchenko <dima.den4enko.ok@gmail.com>
2024-03-22 14:54:44 +0100

    Update

--- PackFramework Builder.ps1
+++ PackFramework Builder.ps1
@@ -6,13 +6,13 @@ do {
     Write-Host "[PackFramework Builder]"
     Write-Host "Select version to build:"
     Write-Host
-    Write-Host "1) 1.20.1 Ultra"
-    Write-Host "2) 1.20.1 Nano"
-    Write-Host "3) 1.19.2 Ultra"
-    Write-Host "2) 1.19.2 Nano"
-    Write-Host "5) 1.18.2 Ultra"
-    Write-Host "6) 1.18.2 Nano"
-    Write-Host "7) All Versions"
+    Write-Host "1) All Versions"
+    Write-Host "2) 1.20.1 Ultra"
+    Write-Host "3) 1.20.1 Nano"
+    Write-Host "4) 1.19.2 Ultra"
+    Write-Host "5) 1.19.2 Nano"
+    Write-Host "6) 1.18.2 Ultra"
+    Write-Host "7) 1.18.2 Nano"
     Write-Host
     Write-Host "8) Exit"
     Write-Host
@@ -21,14 +21,14 @@ do {
     Clear-Host
 
     switch ($input) {
-        "1" { Start-Process -FilePath "$scriptPath\scripts\1.20.1-ultra.bat" -Wait }
-        "2" { Start-Process -FilePath "$scriptPath\scripts\1.20.1-nano.bat" -Wait }
-        "3" { Start-Process -FilePath "$scriptPath\scripts\1.19.2-ultra.bat" -Wait }
-        "4" { Start-Process -FilePath "$scriptPath\scripts\1.19.2-nano.bat" -Wait }
-        "5" { Start-Process -FilePath "$scriptPath\scripts\1.18.2-ultra.bat" -Wait }
-        "6" { Start-Process -FilePath "$scriptPath\scripts\1.18.2-nano.bat" -Wait }
+        "2" { Start-Process -FilePath "$scriptPath\scripts\1.20.1-ultra.bat" -Wait }
+        "3" { Start-Process -FilePath "$scriptPath\scripts\1.20.1-nano.bat" -Wait }
+        "4" { Start-Process -FilePath "$scriptPath\scripts\1.19.2-ultra.bat" -Wait }
+        "5" { Start-Process -FilePath "$scriptPath\scripts\1.19.2-nano.bat" -Wait }
+        "6" { Start-Process -FilePath "$scriptPath\scripts\1.18.2-ultra.bat" -Wait }
+        "7" { Start-Process -FilePath "$scriptPath\scripts\1.18.2-nano.bat" -Wait }
 
-        "7" { 
+        "1" { 
         Start-Process -FilePath "$scriptPath\scripts\1.20.1-ultra.bat"
         Start-Process -FilePath "$scriptPath\scripts\1.20.1-nano.bat"
         Start-Process -FilePath "$scriptPath\scripts\1.19.2-ultra.bat"
