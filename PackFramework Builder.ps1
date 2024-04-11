0f0fe46f5b9e41262f07350c581b131d245a2da1
Dmytro Denchenko <dima.den4enko.ok@gmail.com>
2024-04-11 13:07:06 +0200

    Update PackFramework Builder.ps1

--- PackFramework Builder.ps1
+++ PackFramework Builder.ps1
@@ -1,94 +1,164 @@
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-    Write-Host "[PackFramework Builder]"
-    Write-Host "Select version to build:"
-    Write-Host
-    Write-Host "1) (Don't use!) All Versions"
-    Write-Host "2) Forge 1.20.1 Ultra"
-    Write-Host "3) Forge 1.20.1 Nano"
-    Write-Host "4) Forge 1.19.2 Ultra"
-    Write-Host "5) Forge 1.19.2 Nano"
-    Write-Host "6) Forge 1.18.2 Ultra"
-    Write-Host "7) Forge 1.18.2 Nano"
-    Write-Host
-    Write-Host "8) Exit"
-    Write-Host
+# Define the function to build a modpack
+function Select-Version {
+# Prompt the user to select a version to build
+Write-Host "[PackFramework Builder]" -ForegroundColor Green
+Write-Host "Select version to build:"
+Write-Host
+Write-Host "1) All Forge Versions"
+Write-Host "2) Forge 1.20.1 Ultra"
+Write-Host "3) Forge 1.20.1 Nano"
+Write-Host "4) Forge 1.19.2 Ultra"
+Write-Host "5) Forge 1.19.2 Nano"
+Write-Host "6) Forge 1.18.2 Ultra"
+Write-Host "7) Forge 1.18.2 Nano"
+Write-Host
+Write-Host "0) Exit"
+Write-Host
 
-    $type = Read-Host -Prompt "Enter number: "
-    Clear-Host
+# Read the user's input
+$type = Read-Host -Prompt "Enter number: "
+Clear-Host
 
-    switch ($type) {
+# Switch statement to handle the user's input
+switch ($type) {
     "2" {
-            $mcversion = "1.20.1"
-            $modpacktype = "ultra"
-            $modloader = "forge"
-        }
+        # Set the variables for the modpack to be built
+        $mcversion = "1.20.1"
+        $modpacktype = "ultra"
+        $modloader = "forge"
+
+        # Build the modpack
+        Build-Modpack
+        Select-Version
+    }
     "3" {
-            $mcversion = "1.20.1"
-            $modpacktype = "nano"
-            $modloader = "forge"
-        }
+        # Set the variables for the modpack to be built
+        $mcversion = "1.20.1"
+        $modpacktype = "nano"
+        $modloader = "forge"
+
+        # Build the modpack
+        Build-Modpack
+        Select-Version
+    }
     "4" {
-            $mcversion = "1.19.2"
-            $modpacktype = "ultra"
-            $modloader = "forge"
-        }
+        # Set the variables for the modpack to be built
+        $mcversion = "1.19.2"
+        $modpacktype = "ultra"
+        $modloader = "forge"
+
+        # Build the modpack
+        Build-Modpack
+        Select-Version
+    }
     "5" {
-            $mcversion = "1.19.2"
-            $modpacktype = "nano"
-            $modloader = "forge"
-        }
+        # Set the variables for the modpack to be built
+        $mcversion = "1.19.2"
+        $modpacktype = "nano"
+        $modloader = "forge"
+
+        # Build the modpack
+        Build-Modpack
+        Select-Version
+    }
     "6" {
-            $mcversion = "1.18.2"
-            $modpacktype = "ultra"
-            $modloader = "forge"
-        }
+        # Set the variables for the modpack to be built
+        $mcversion = "1.18.2"
+        $modpacktype = "ultra"
+        $modloader = "forge"
+
+        # Build the modpack
+        Build-Modpack
+        Select-Version
+    }
     "7" {
-            $mcversion = "1.18.2"
-            $modpacktype = "nano"
-            $modloader = "forge"
-        }
+        # Set the variables for the modpack to be built
+        $mcversion = "1.18.2"
+        $modpacktype = "nano"
+        $modloader = "forge"
 
-        "1" { 
-        Start-Process -FilePath "$scriptPath\scripts\1.20.1-ultra.bat"
-        Start-Process -FilePath "$scriptPath\scripts\1.20.1-nano.bat"
-        Start-Process -FilePath "$scriptPath\scripts\1.19.2-ultra.bat"
-        Start-Process -FilePath "$scriptPath\scripts\1.19.2-nano.bat"
-        Start-Process -FilePath "$scriptPath\scripts\1.18.2-ultra.bat"
-        Start-Process -FilePath "$scriptPath\scripts\1.18.2-nano.bat"
+        # Build the modpack
+        Build-Modpack
+        Select-Version
+    }
+
+    "1" { 
+        $mcversion = "1.20.1"
+        $modpacktype = "ultra"
+        $modloader = "forge"
+        Build-Modpack
+
+        $mcversion = "1.20.1"
+        $modpacktype = "nano"
+        $modloader = "forge"
+        Build-Modpack
+
+        $mcversion = "1.19.2"
+        $modpacktype = "ultra"
+        $modloader = "forge"
+        Build-Modpack
+
+        $mcversion = "1.19.2"
+        $modpacktype = "nano"
+        $modloader = "forge"
+        Build-Modpack
+
+        $mcversion = "1.18.2"
+        $modpacktype = "ultra"
+        $modloader = "forge"
+        Build-Modpack
+
+        $mcversion = "1.18.2"
+        $modpacktype = "nano"
+        $modloader = "forge"
+        Build-Modpack
+
         exit 
-            }
-        "8" { exit }
-        default { 
-            Write-Host "Invalid Input. Please try one more time." -ForegroundColor Red
-                }
-                    }
-# Start
-$outputPath = "$scriptPath\beta\$modloader\$mcversion\$modpacktype"
-Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Building $mcversion-$modpacktype..."
-
-# Clean old files
-Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Cleaning files..."
-Remove-Item -Path "$outputPath\*.*" -Recurse
-New-Item -Path "$outputPath" -ItemType Directory -Force
-
-# Merge
-Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Merging..."
-Copy-Item -Path "$scriptPath\source\$modloader\shared\nano\*" -Destination "$outputPath" -Recurse -Force
-if ($modpacktype -eq 'ultra') {
-    Copy-Item -Path "$scriptPath\source\$modloader\shared\ultra\*" -Destination "$outputPath" -Recurse -Force
+    }
+    "0" { exit }
+    default { 
+        # Display an error message if the user entered an invalid input
+        Write-Host "Invalid Input. Please try one more time." -ForegroundColor Red
+        Select-Version
+    }
 }
-Copy-Item -Path "$scriptPath\source\$modloader\$mcversion\nano\*" -Destination "$outputPath" -Recurse -Force
 
-# Copy Changelog
-Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Copying Changelog..."
-Copy-Item "$scriptPath\CHANGELOG.md" "$outputPath\config\fancymenu\assets\changelog.md"
+}
+function Build-Modpack {
 
-# Update
-Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Updating..."
-Set-Location "$outputPath"
-& packwiz refresh
-& packwiz update --all -y
+    # Set the output path based on the modloader, Minecraft version, and modpack type
+    $outputPath = "$scriptPath\beta\$modloader\$mcversion\$modpacktype"
 
-# Finish
-Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Done!" 
+    # Display a message indicating that the build is starting
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Building $mcversion-$modpacktype..."
+
+    # Clean up any old files in the output path
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Cleaning files..."
+    Remove-Item -Path "$outputPath\*.*" -Recurse
+    New-Item -Path "$outputPath" -ItemType Directory -Force
+
+    # Merge the necessary files into the output path
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Merging..."
+    Copy-Item -Path "$scriptPath\source\$modloader\shared\nano\*" -Destination "$outputPath" -Recurse -Force
+    if ($modpacktype -eq 'ultra') {
+        Copy-Item -Path "$scriptPath\source\$modloader\shared\ultra\*" -Destination "$outputPath" -Recurse -Force
+    }
+    Copy-Item -Path "$scriptPath\source\$modloader\$mcversion\nano\*" -Destination "$outputPath" -Recurse -Force
+
+    # Copy the changelog to the output path
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Copying Changelog..."
+    Copy-Item "$scriptPath\CHANGELOG.md" "$outputPath\config\fancymenu\assets\changelog.md"
+
+    # Update the modpack using packwiz
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Updating..."
+    Set-Location "$outputPath"
+    & packwiz refresh
+    & packwiz update --all -y
+
+    # Display a message indicating that the build is complete
+    Write-Host "[$(Get-Date -Format 'HH:mm:ss')] Done!"
+
+}
+Select-Version
